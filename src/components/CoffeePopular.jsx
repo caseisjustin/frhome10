b380dc118316ca7d4c2f7372e90210926f9522fa
subject <hotspot>
2024-11-26 11:27:25 +0000

    Coffee Poplr finished

--- src/components/CoffeePopular.jsx
+++ src/components/CoffeePopular.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./CoffeePopular.scss"
+import BagOfCoffee from "../assets/Rectangle 2.png"
 
 const PopularCof = () => {
 
@@ -7,35 +9,35 @@ const PopularCof = () => {
             <section className="popular_prdcs">
                 <div className="container">
                     <div className="popular_prdcs_wrp">
-                        <div>
+                        <div className="poplr_prdc_desc">
                             <h5>Popular Product</h5>
                             <h4>Coffee popular Product</h4>
                         </div>
-                        <div>
-                            <div>
-                                <div>
-                                    <img src="" alt="PHOTO" />
+                        <div className="pplr_prd_card_wrp">
+                            <div className="pplr_prd_cards">
+                                <div className="pplr_prd_card">
+                                    <img src={BagOfCoffee} alt="PHOTO" />
                                     <h5>COFFEE * * * * *</h5>
                                     <b>BRAZIL COFFEE GRED</b>
                                     <p>PRICE - $220.00/ $360</p>
                                     <button>Add to Cart</button>
                                 </div>
-                                <div>
-                                    <img src="" alt="PHOTO" />
+                                <div className="pplr_prd_card">
+                                    <img src={BagOfCoffee} alt="PHOTO" />
                                     <h5>COFFEE * * * * *</h5>
                                     <b>BRAZIL COFFEE GRED</b>
                                     <p>PRICE - $220.00/ $360</p>
                                     <button>Add to Cart</button>
                                 </div>
-                                <div>
-                                    <img src="" alt="PHOTO" />
+                                <div className="pplr_prd_card">
+                                    <img src={BagOfCoffee} alt="PHOTO" />
                                     <h5>COFFEE * * * * *</h5>
                                     <b>BRAZIL COFFEE GRED</b>
                                     <p>PRICE - $220.00/ $360</p>
                                     <button>Add to Cart</button>
                                 </div>
                             </div>
-                            <div>
+                            <div className="pplr_prd_btn">
                                 <button>
                                     VIEW ALL PRODUCT
                                 </button>
